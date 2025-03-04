1e2dd78a97d09c287974889713c235ca2e1e74ff
Akashraghav2003 <akash.si8273@gmail.com>
2025-03-04 11:42:33 +0530

    [Akash singh] Resolve merge conflicts

--- RepositoryLayer/obj/Debug/net6.0/RepositoryLayer.AssemblyInfo.cs
+++ RepositoryLayer/obj/Debug/net6.0/RepositoryLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e6ac6531acae6f0128b3392250704b0b74ecbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5560f3d409b0e9e46500a3bcc7ed3dfa0f2f80d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
